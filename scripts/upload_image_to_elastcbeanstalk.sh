6c9da76221df5b14011278827c9889f6c0e8f7c8
Usama M. Ali <eng.usama.muhammad@gmail.com>
2019-01-05 23:54:12 +0400

    CI

---
+++ scripts/upload_image_to_elastcbeanstalk.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+# Variables
+DOCKER_TAG=$1
+DOCKERRUN_FILE="Dockerrun.aws.json"
+DOCKER_CONFIG="/home/travis/.docker/config.json"
+EB_BUCKET=$2
+EB_ENV=$3
+PREFIX="deploy/$DOCKER_TAG"
+APP_NAME=$4
+DEPLOYMENT_REGION=$5
+IMAGE_NAME=$6
+DEPLOYMENT_ENV_NAME=$7
+DOCKER_USERNAME=$8
+DOCKER_REPOSITORY=$9
+DOCKER_PASSWORD=${10}
+DOCKER_EMAIL=${11}
+DOCKER_IMAGE="$DOCKER_USERNAME/$DOCKER_REPOSITORY"
+
+sleep 30
+echo "::::: Creating Dockerrun.aws.json file :::::"
+# Replace vars in the DOCKERRUN_FILE 
+cat "$DOCKERRUN_FILE" \
+  | sed 's||'$EB_BUCKET'|g' \
+  | sed 's||'$DOCKER_IMAGE'|g' \
+  | sed 's||'$DOCKER_TAG'|g' \
+  > $DOCKERRUN_FILE
+sleep 30
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$PREFIX/$DOCKERRUN_FILE
+sleep 30
+echo "::::: Creating new Elastic Beanstalk version :::::"
+# Run aws command to create a new EB application with label
+aws elasticbeanstalk create-application-version \
+    --region=$DEPLOYMENT_REGION \
+    --application-name $APP_NAME \
+  --version-label $DOCKER_TAG \
+    --source-bundle S3Bucket=$EB_BUCKET,S3Key=$PREFIX/$DOCKERRUN_FILE
+  
+sleep 30  
+echo "::::: Updating Elastic Beanstalk environment :::::"
+aws elasticbeanstalk update-environment \
+  --environment-id $EB_ENV \
+  --environment-name $DEPLOYMENT_ENV_NAME \
+  --application-name $APP_NAME \
+  --version-label $DOCKER_TAG
+echo "::::: Removing file :::::"
+sleep 30  
+rm $DOCKERRUN_FILE
